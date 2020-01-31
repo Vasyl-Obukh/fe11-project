3a4d8bbc396f750f0a1ad445186dd71a68a42b35
Vasyl Obukh <Vasyl_Obukh@epam.com>
2020-01-31 12:22:28 +0200

    change cloud service import to module import

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategoriesModule } from './categories/categories.module';
 import { ArticlesModule } from './articles/articles.module';
+import { CloudModule } from './cloud/cloud.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/blog'), CategoriesModule, ArticlesModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/blog'), CategoriesModule, ArticlesModule, CloudModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/articles/articles.module.ts
+++ server/src/articles/articles.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CloudService } from '../cloud/cloud.service';
+import { CloudModule } from '../cloud/cloud.module';
 import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
 import { ArticleSchema } from './schemas/article.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: 'Article', schema: ArticleSchema}])],
+  imports: [MongooseModule.forFeature([{name: 'Article', schema: ArticleSchema}]), CloudModule],
   controllers: [ArticlesController],
-  providers: [ArticlesService, CloudService],
+  providers: [ArticlesService],
 })
 export class ArticlesModule {}
